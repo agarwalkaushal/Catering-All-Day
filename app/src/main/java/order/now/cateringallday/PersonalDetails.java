a3a4e0fd7ee4bc412badad4c2dc9b720f343b6f2
Yaswant Narayan <yoyaswant@gmail.com>
2017-09-09 01:01:44 +0530

    Added PersonalDetails Layout - yaswant

---
+++ app/src/main/java/order/now/cateringallday/PersonalDetails.java
@@ -0,0 +1,18 @@
+package order.now.cateringallday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class PersonalDetails extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+
+
+    }
+}
